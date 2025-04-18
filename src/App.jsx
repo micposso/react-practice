c7e3efadb4788e47f93f8d846ad6941c181bc21e
micposso <micposso@gmail.com>
2025-04-18 13:40:41 -0400

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+// src/App.jsx
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+import "./App.css";
 
-import './App.css'
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import TaskHistory from "./components/TaskHistory";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+
+  const addTask = (text) => {
+    setTasks([...tasks, { id: Date.now(), text }]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    const taskToComplete = tasks.find((task) => task.id === id);
+    if (taskToComplete) {
+      setTasks(tasks.filter((task) => task.id !== id));
+      setCompletedTasks([...completedTasks, taskToComplete]);
+    }
+  };
+
+  const editTask = (id, newText) => {
+    setTasks(
+      tasks.map((task) => (task.id === id ? { ...task, text: newText } : task))
+    );
+  };
 
   return (
     <Container maxWidth="lg">
       <Box sx={{ bgcolor: "#cfe8fc", height: "100vh", width: "100vh" }}>
         <Typography variant="h1">ToDo List</Typography>
-        {/* comment */}
         <Box className="bg-color" component="section" sx={{ p: 2 }}>
-          <TextField sx={{ width: "100%"}} id="outlined-basic" label="Type Task" variant="outlined" />
+          <TaskInput onAddTask={addTask} />
+        </Box>
+        <Box sx={{ p: 2 }}>
+          <TaskList
+            tasks={tasks}
+            onDelete={deleteTask}
+            onComplete={completeTask}
+            onEdit={editTask}
+          />
+        </Box>
+        <Box sx={{ p: 2 }}>
+          <TaskHistory completedTasks={completedTasks} />
         </Box>
       </Box>
     </Container>
   );
-}
+};
 
-export default App
+export default App;
